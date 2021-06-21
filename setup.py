067a6284316d3094aac325b2632fbc04518fc395
Dora Li <dongshunzi.c@gmail.com>
2021-06-20 23:18:20 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     python_requires=REQUIRES_PYTHON,
     url=URL,
     packages=['trasig'],
-    entry_points={'console_scripts': ['trasig=trasig.trasig:main']},
+    entry_points={'console_scripts': ['trasig=trasig.main']},
     install_requires=REQUIRED,
     include_package_data=True,
     license='MIT',
