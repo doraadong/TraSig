93332535caba26c6a726ccf0430055713b64ac11
Dora Li <dongshunzi.c@gmail.com>
2021-12-21 23:42:06 -0500

    Add alignment module

--- trasig/__init__.py
+++ trasig/__init__.py
@@ -4,6 +4,6 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(dir_path)
 
-__all__ = ['main', 'trasig', 'utils']
+__all__ = ['main', 'trasig', 'alignment', 'utils']
 for i in __all__:
     __import__(i)
